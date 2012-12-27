43a9d0b5d74733a291c0a48345ded5abf6ad7df4
Daniel Holmstrom <holmstrom.daniel@gmail.com>
2012-12-27 20:50:59 +0100

    Moved preparation of arguments to separate method
    This is required for asdict and fromdict to behave the same

--- iteralchemy/utils.py
+++ iteralchemy/utils.py
@@ -7,23 +7,8 @@ from sqlalchemy.orm.collections import InstrumentedList
 
 from iteralchemy import constants
 
-
-def asdict(self, exclude=None, exclude_underscore=None, follow=None):
-    """Get a dict from a model
-
-    This method can also be set on a class directly
-
-    :param follow: List or dict of relationships that should be followed.
-    If the parameter is a dict the value should be a dict of keyword
-    arguments.
-    :param exclude: List of properties that should be excluded, will be
-    merged with self.asdict_exclude.
-    :param exclude_underscore: Overides self.exclude_underscore if set.
-
-    :raises: ValueError if follow contains a non-existent relationship
-
-    :returns: dict
-    """
+def _prepare_arguments(self, exclude=None, exclude_underscore=None,
+        follow=None):
 
     if follow == None:
         follow = []
@@ -49,6 +34,28 @@ def asdict(self, exclude=None, exclude_underscore=None, follow=None):
     if getattr(self, 'asdict_exclude_underscore', True):
         # Exclude everything starting with underscore
         exclude += [k for k in self.__mapper__._props if k[0] == '_']
+
+    return exclude, exclude_underscore, follow, relations, columns, synonyms
+
+def asdict(self, exclude=None, exclude_underscore=None, follow=None):
+    """Get a dict from a model
+
+    This method can also be set on a class directly
+
+    :param follow: List or dict of relationships that should be followed.
+    If the parameter is a dict the value should be a dict of keyword
+    arguments.
+    :param exclude: List of properties that should be excluded, will be
+    merged with self.asdict_exclude.
+    :param exclude_underscore: Overides self.exclude_underscore if set.
+
+    :raises: ValueError if follow contains a non-existent relationship
+
+    :returns: dict
+    """
+
+    exclude, exclude_underscore, follow, relations, columns, synonyms =\
+        _prepare_arguments(self, exclude, exclude_underscore, follow)
 
     data = dict([(k, getattr(self, k)) for k in columns + synonyms\
             if k not in exclude])
