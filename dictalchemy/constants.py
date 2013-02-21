daa17809eb427b7a2f15129f2e726952bea2b75a
Daniel Holmstrom <holmstrom.daniel@gmail.com>
2013-02-21 19:57:27 +0100

    Docs

--- dictalchemy/constants.py
+++ dictalchemy/constants.py
@@ -5,5 +5,6 @@ from __future__ import absolute_import, division
 default_exclude = None
 """Default value for asdict_exclude"""
 default_exclude_underscore = True
-"""Default value for fromdict_allow_pk if not set"""
+"""Default value for exclude_underscore if not set"""
 default_fromdict_allow_pk = False
+"""Default value for fromdict_allow_pk if not set"""
